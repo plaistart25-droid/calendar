c120827500a2f57b1eeeb2cd4845a0fe76d98750
DanielK <saponembigas@gmail.com>
2025-11-06 18:06:51 +0700

    fix async

--- src/main/java/dev/kuklin/kworkcalendar/KworkcalendarApplication.java
+++ src/main/java/dev/kuklin/kworkcalendar/KworkcalendarApplication.java
@@ -3,11 +3,13 @@ package dev.kuklin.kworkcalendar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableAsync
 public class KworkcalendarApplication {
 
 	public static void main(String[] args) {
